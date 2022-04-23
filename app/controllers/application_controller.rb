e746bfd78b4793f6ac083032fa28d1ee3d0d0a82
JonCabot <jfc0053@gmail.com>
2022-04-23 14:05:34 -0500

    commit tree da8eff2a01a409315bdff26485c5288d87a80147

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  #routes
+  get '/bakeries' do
+    bakery = Bakery.all
+    bakery.to_json
+  end
 
-  # add routes
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(only: [:name], include: {
+      baked_goods: { only: [:name, :price] }
+    })
+  end
 
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.by_price
+    baked_goods.to_json
+  end
+ 
+  get '/baked_goods/most_expensive' do
+    baked_goods = BakedGood.by_price.first
+    baked_goods.to_json
+  end
 end
--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
+
 class BakedGood < ActiveRecord::Base
-  # add association macro here
+  belongs_to :bakery
+
+  def self.by_price
+    self.all.order(price: :desc)
+  end
+
 end
